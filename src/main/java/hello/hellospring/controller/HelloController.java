64632e8e8383b30a860e030eddfd6c6f88fa384e
mars.m <moonhb4901@gmail.com>
2022-03-14 23:27:43 +0900

    init

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
